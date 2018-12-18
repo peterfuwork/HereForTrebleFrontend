6aa7c2d3e735ba9cd5b2b54c4ce085690fde1ae7
s0njablade <sonjita.chacon@gmail.com>
2018-12-18 14:39:48 -0700

    created/updated profile page. added styling and javascript. created carousel, and placeholder profile pic. added profile copy - name, email, added placeholder images PUG! for carousel.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  slideConfig = {
+    'slidesToShow': 4,
+    'slidesToScroll': 1,
+    'autoplay': true,
+    'dots': true,
+    'speed': 300,
+    'vertical': true,
+    'responsive': [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 885,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: false
+        }
+      }
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
